a787b59fc9f554ea159ceb53150183f966469203
ASUVATH-MS <76156697+ASUVATH-MS@users.noreply.github.com>
2021-08-25 18:22:52 +0530

    Add files via upload

---
+++ day2/index.js
@@ -0,0 +1,27 @@
+let food=[
+    {
+        name:"Shawarma",
+        price:120,
+        quantity:5,
+    }
+    ,
+    {
+        name:"Grill chic",
+        price:320,
+        quantity:5,
+    }
+    ,
+    {
+        name:"Dosa",
+        price:60,
+        quantity:5,
+    }
+]
+
+
+for(let num=0;num<food.length;num++){
+    console.log(food[num]);
+}
+
+
+//name,price,quantity
